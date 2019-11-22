0b23e4a36e32fbc2a6a73c5154372b1116f638c1
Utsavkumar Lal <utsavkumar.lal@us.brainscale.com>
2019-11-22 20:30:39 +0530

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,50 +1,34 @@
 ﻿
 using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public override char GetLetterGrade(double averageGrade)
-        {
-            if(Students.Count<5)
-            {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            }
-            var cntGreaterThanGiven = 0;
-            foreach(var student in Students)
-            {
-                if(averageGrade>student.AverageGrade)
-                {
-                    cntGreaterThanGiven++;
-                }
-            }
-            var thresholdCount20 = Students.Count * 0.2;
-            var thresholdCount40 = Students.Count * 0.4;
-            var thresholdCount60 = Students.Count * 0.6;
-            var thresholdCount80 = Students.Count * 0.8;
-            if (cntGreaterThanGiven<=thresholdCount20)
-            {
-                return 'A';
-            }
-            if (cntGreaterThanGiven <= thresholdCount40)
-            {
-                return 'B';
-            }
-            if (cntGreaterThanGiven <= thresholdCount60)
-            {
-                return 'C';
-            }
-            if(cntGreaterThanGiven <= thresholdCount80)
-            {
-                return 'D';
-            }
-            return 'F';
-        }
-
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var threshold = (int) Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            return 'F';
+        }
+
+
     }
 }
